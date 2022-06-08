9bf3cc8e32a2efcd51e4fd94132c239d742e8283
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-07 20:23:18 -0400

    Mocha/chai test files

---
+++ test/syncBreedsTest.js
@@ -0,0 +1,22 @@
+const assert = require('chai').assert;
+const breedDetails = require('../syncBreeds.js');
+
+describe("breedDetails", () => {
+  it('returns breed details for the Bombay breed', () => {
+    const expected = "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.";
+    const bombay = breedDetails('Bombay');
+    assert.equal(bombay, expected);
+  });
+
+  it('returns breed details for the Balinese breed', () => {
+    const exp = "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.";
+    const balinese = breedDetails("Balinese");
+    assert.equal(balinese, exp);
+  })
+
+  it('returns undefined for Siamese', () => {
+    const expected = undefined;
+    const siamese = breedDetails('Siamese');
+    assert.equal(siamese, expected);
+  })
+});
